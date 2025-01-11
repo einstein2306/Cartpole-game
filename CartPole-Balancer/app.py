5474899e259020f6f4303c8fc2f8a3fb4755017e
einstein2306 <pasinayaswanth2004@gmail.com>
2025-01-11 11:38:50 +0530

    Add files via upload

---
+++ CartPole-Balancer/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template
+from models.load_model import CartpoleModel
+from flask_socketio import SocketIO
+from models.user import CartPoleGame
+import threading
+import time
+
+
+game = CartPoleGame()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cartpole123'
+socket = SocketIO(app)
+
+gif_data = None
+
+def generate():
+        global gif_data , len_frames
+
+        while True:
+             gif_data, len_frames = CartpoleModel()
+             socket.emit("animation_gif",{"gif_data": gif_data})
+             time.sleep(10)
+
+threading.Thread(target=generate,daemon=True).start()
+
+@socket.on("connect")
+def connection():
+     print("user connected.")
+    
+@app.route('/')
+def main():
+    return render_template('Main.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/Algorithm')
+def algorithm():
+    return render_template('algorithm.html')
+
+
+
+@app.route('/AiMode')
+def AIMode():
+    if gif_data is None:
+         mess = 'Program running has been started.'
+         return render_template('AiMode.html',mess=mess)
+
+    return render_template('AiMode.html',gif_data=gif_data)
+
+
+@app.route('/UserMode')
+def UserMode():
+     return render_template("usermode.html")
+
+
+@socket.on("action")
+def step(data):
+    """
+    Perform a step in the environment based on the user's action.
+    """
+    action = 0 if data['action'] == "left" else 1  # Default to 0 if no action is provided
+    frame, done = game.step(action)
+    socket.emit('frame',{'frame':frame,'done':done})
+
+@socket.on("reset")
+def reset():
+    """
+    Reset the CartPole environment.
+    """
+    frame = game.reset()
+    socket.emit('frame',{'frame':frame,'done':False})
+
+
+
+
+
+if __name__ == "__main__":
+    
+    socket.run(app,debug=True)
+    
---
+++ CartPole-Balancer/models/load_model.py
@@ -0,0 +1,79 @@
+import os
+
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+#import hashlib
+import gymnasium as gym
+from stable_baselines3 import DQN
+from io import BytesIO
+from PIL import Image
+import base64
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv
+#import numpy as np
+
+
+def CartpoleModel():
+    env = gym.make("CartPole-v1",render_mode="rgb_array")
+    env = Monitor(env)
+    env = DummyVecEnv([lambda:env])
+    model = DQN.load("logs/dqn/CartPole-v1_1/best_model.zip", env=env)
+
+    frames=[]
+    
+    
+    
+    for _ in range(1):
+        obs = env.reset()
+
+        while True:
+
+            action, _ = model.predict(obs,deterministic=False)
+            obs,_ , done, _  = env.step(action)
+            #angle = abs(obs[0][2])
+            
+            frames.append(env.render())
+        
+        
+            if done :
+                break
+    
+    buffer = BytesIO()   
+    images = [Image.fromarray(frame) for frame in frames]
+    images[0].save(buffer,format="GIF",save_all=True,append_images=images[1:],duration=50,loop=1)
+    buffer.seek(0)
+    gif_data = base64.b64encode(buffer.read()).decode('utf-8')
+        
+    return gif_data , len(frames)
+
+
+
+"""
+
+if __name__ == "__main__":
+
+
+    global gif_data , total_frames
+    data = None
+    while True:
+        
+        new_data , new_total_frames = CartpoleModel()
+        new_data = hashlib.md5(new_data).hexdigest()
+        if data is None or data != new_data:
+            data = new_data
+            total_frames = new_total_frames
+            print("It is updating..")
+    
+        else:
+            print('it is same')
+            
+"""
+
+
+
+
+        
+    
+
+        
